3685e3511d626a77b32a2d3b07d9743f41c950e5
RoshHoul <kabadzhg@uni.coventry.ac.uk>
2016-03-08 15:29:22 +0000

    Scene without textures

---
+++ Assets/Rooms Assets/DoorScript/DoorAnimation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAnimation : MonoBehaviour
+{
+    bool isDoorOpen;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            this.GetComponent<Animator>().SetTrigger("Open");
+            isDoorOpen = true;
+        }
+            
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            this.GetComponent<Animator>().SetTrigger("Close");
+             isDoorOpen = false;
+        }
+    }
+
+}
